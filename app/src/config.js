1661e894304f3d205da6c4cd245cb329097709b2
zenercurrent <ISAACGOHJR02.19@ichat.sp.edu.sg>
2021-09-04 16:03:19 +0800

    mobot webapp

---
+++ app/src/config.js
@@ -0,0 +1,13 @@
+// const config = {
+// 	apiKey: process.env.apiKey,
+// 	authDomain: process.env.authDomain,
+// 	databaseURL: process.env.databaseURL
+// };
+
+const config = {
+	apiKey: "AIzaSyC8XpYi2ozXZwWibLjUcVf6ZCJ8sVw6AMg",
+	authDomain: "mobot-app-39805.firebaseapp.com",
+	databaseURL: "https://mobot-app-39805-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
+
+export default config;
